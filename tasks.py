0c2305db6c6792f624cf09a9134aaa090c82d5c1
stepan <stepanperlov@gmail.com>
2016-07-14 15:25:33 +0300

    Add mezzo task, for copy project to mezzo

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 from invoke import task
+from invoke.util import cd
 import jschema
 
 
@@ -12,3 +13,12 @@ def pip(ctx):
 def doc(ctx):
     ctx.run("./setup.py build_sphinx")
     ctx.run("./setup.py upload_sphinx")
+
+@task
+def mezzo(ctx):
+    ctx.run("mkdir -p build/jschema")
+    ctx.run("cp -R jschema setup.py build/jschema")
+    with cd("build"):
+        ctx.run("tar cf jschema.tar.gz jschema")
+        ctx.run("mv jschema.tar.gz /opt/mezzo/dependencies")
+        ctx.run("rm -rf jschema")
