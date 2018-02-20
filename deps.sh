c61b0ac664bd9c49feeb6406cde940e357e53b2b
Stepan Perlov <stepanperlov@gmail.com>
2018-02-20 20:33:41 +0300

    Remove unused files

--- deps.sh
+++ deps.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
-
-# jschema dependencies
-wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
-sudo pip install PyYAML
-sudo pip install jinja2
-
 # develop dependencies
 # invoke - run tasks.py file
 # twine - upload to pip
-# Sphinx - documentation toolkit for python
-# sphinx_rtd_theme - documentation theme
-# sphinx-pypi-upload - upload doc to pip
 sudo pip install invoke
 sudo pip install twine
-sudo pip install Sphinx
-sudo pip install sphinx_rtd_theme
-sudo pip install sphinx-pypi-upload
